b2a54266a7af1afcc3ec857530fb750689856da4
flypem12-pixel <flypem12@gmail.com>
2026-01-05 16:14:51 +0000

    Criei as variaveis e area para entrada e saida de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,77 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado_1;
+  char codigo_1[5], codigo_2[5];
+  char cidade_1[20], cidade_2[20];
+  unsigned long int populacao_1; 
+  int turismo_1;
+  float area_1;
+  long double PIB_1;
+
+  char  estado_2;
+  unsigned long int populacao_2;
+  int turismo_2;
+  float area_2;
+  long double PIB_2;
 
   // Área para entrada de dados
+  printf("Digite uma letra para o Estado (A-H): ");
+  scanf(" %c", &estado_1);
+
+  printf("Digite o código do Estado escolhido (Ex: X001): ");
+  scanf("%s", codigo_1);
+
+  printf("Digite o nome da cidade: ");
+  scanf("%s", cidade_1);
+
+  printf("Digite o número da população: ");
+  scanf("%lu", &populacao_1);
+
+  printf("Digite a quantidade de pontos turísticos: ");
+  scanf("%d", &turismo_1);
+
+  printf("Digite a área(km²): ");
+  scanf("%f", &area_1);
+
+  printf("Digite o PIB: ");
+  scanf("%Lf", &PIB_1);
+
+    printf("------------------------------- \n");
+
+  printf("Digite uma letra para o Estado 2 (A-H): ");
+  scanf(" %c", &estado_2);
+
+  printf("Digite o código do Estado 2 escolhido (Ex: X001): ");
+  scanf("%s", codigo_2);
+
+  printf("Digite o nome da cidade 2: ");
+  scanf("%s", cidade_2);
+
+  printf("Digite o número da população: ");
+  scanf("%lu", &populacao_2);
+
+  printf("Digite a quantidade de pontos turísticos: ");
+  scanf("%d", &turismo_2);
+
+  printf("Digite a área(km²): ");
+  scanf("%f", &area_2);
+
+  printf("Digite o PIB: ");
+  scanf("%Lf", &PIB_2);
+
+  printf("========================================\n");
 
   // Área para exibição dos dados da cidade
+  printf("Carta 1:\n");
+  printf("Estado: %c\n Código: %s\n Cidade: %s\n ", estado_1, codigo_1, cidade_1);
+  printf("População: %lu\n Pontos Turísticos: %d\n Área: %.3f\n PIB: %.2Lf\n", populacao_1, turismo_1, area_1, PIB_1);
 
+  printf("-------------------------------\n");
+
+  printf("Carta 2:\n");
+  printf("Estado: %c\n Código: %s\n Cidade: %s\n ", estado_2, codigo_2, cidade_2);
+  printf("População: %lu\n Pontos Turísticos: %d\n Área: %.3f\n PIB: %.2Lf\n", populacao_2, turismo_2, area_2, PIB_2);
+  
 return 0;
 } 
